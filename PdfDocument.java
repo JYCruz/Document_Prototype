6918bf084b1844fb6125fd3e8fd06c464a9bdb74
Josh Yvan Cruz <josh.cruz@neu.edu.ph>
2025-04-14 07:25:49 +0800

    Update PdfDocument.java

--- PdfDocument.java
+++ PdfDocument.java
@@ -3,3 +3,27 @@ class PdfDocument implements Document {
     private String author;
     private int pageCount;
     private String name;
+
+    public PdfDocument(String fileName, String author, int pageCount, String name) {
+        this.fileName = fileName;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.name = name;
+    }
+
+    @Override
+    public Document clone() {
+        return new PdfDocument(this.fileName, this.author, this.pageCount, this.name);
+    }
+
+    @Override
+    public void open() {
+        System.out.println("Opening PDF Document: " + fileName + " by " + author + " (" + pageCount + " pages)");
+        System.out.println("Type: PDF, File: " + fileName + ", Author: " + author + ", Pages: " + pageCount);
+    }
+
+    @Override
+    public String getType() {
+        return "PDF";
+    }
+}
